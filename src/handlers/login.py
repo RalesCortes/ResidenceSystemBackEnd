d817ed9150aa5f5e4a3ee09c845e38aa28dae4d0
Ricardo Cortes <32460439+RalesCortes@users.noreply.github.com>
2024-04-17 21:01:03 -0600

    enums incomplete

--- src/handlers/login.py
+++ src/handlers/login.py
@@ -1,12 +1,11 @@
 from aws_lambda_powertools.utilities.data_classes import event_source, APIGatewayProxyEventV2
 import boto3
+from src.utils.enums import Table
 
-
-dynamodb = boto3.resource('dynamodb', config=my_config)
 @event_source(data_class=APIGatewayProxyEventV2)
 def handler(event: APIGatewayProxyEventV2, context):
     dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table(TABLE_NAME)
+    table = dynamodb.Table(Table.)
 
     return {}
 
---
+++ src/utils/enums.py
@@ -0,0 +1,5 @@
+from enum import Enum
+
+# class syntax
+class Table(Enum):
+    USER = "USER"
