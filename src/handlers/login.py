d3b713474199565b6124ddd5687e3322d39ba28a
Ricardo Cortes <32460439+RalesCortes@users.noreply.github.com>
2024-04-21 01:44:49 -0600

    ready to access_token

--- src/handlers/login.py
+++ src/handlers/login.py
@@ -1,3 +1,5 @@
+from jose import JWTError, jwt
+from datetime import datetime, timedelta
 from aws_lambda_powertools.utilities.data_classes import event_source, APIGatewayProxyEventV2
 from aws_lambda_powertools.utilities.parser import parse,ValidationError
 from src.utils.enums import  ResponseType
@@ -5,6 +7,11 @@ from src.models.request import LoginRequestModel
 from src.models.response import ResponseModel
 from src.datasources.dynamo_manager import DynamoManager
 from src.utils.exceptions import DynamoException
+
+
+SECRET_KEY = "e5747de9a49342cfb7fafd0c05eb1508"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 @event_source(data_class=APIGatewayProxyEventV2)
 def handler(event: APIGatewayProxyEventV2, context):
@@ -17,8 +24,12 @@ def handler(event: APIGatewayProxyEventV2, context):
                                                             user=login_request_model.user, 
                                                             password= login_request_model.password, 
                                                             sub_division= login_request_model.subdivision)
-        
-        return ResponseModel(body = user_data, type = ResponseType.SUCCESS).make_response()        
+        response = {
+            "user_data": user_data,
+            "access_token": create_access_token(user_data)
+        }
+
+        return ResponseModel(body = response, type = ResponseType.SUCCESS).make_response()        
     except ValidationError as e:
         return ResponseModel(body = {"message":str(e)} , type = ResponseType.CUSTOM_ERROR).make_response()
     except DynamoException as e:
@@ -27,3 +38,15 @@ def handler(event: APIGatewayProxyEventV2, context):
         return ResponseModel(body = {"message":str(e)}, type = ResponseType.ERROR).make_response()
 
     
+
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    
+    to_encode.update({"exp": expire})
+
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+    return encoded_jwt
